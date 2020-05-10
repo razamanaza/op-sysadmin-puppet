4a3b0a1eec4f6de47d6bf85a399f8c7bb9a3fca6
root <root@mgmt-b.foo.org.nz>
2020-05-11 09:27:28 +1200

    Changed nrp.conf to template to add host dependent thresholds

--- modules/nrpe_server/manifests/config.pp
+++ modules/nrpe_server/manifests/config.pp
@@ -1,13 +1,28 @@
 class nrpe_server::config {
+  if $hostname == 'mgmt-b' {
+    $check_total_procs = "/usr/lib/nagios/plugins/check_procs -w 180 -c 220"
+    $check_load = "/usr/lib/nagios/plugins/check_load -r -w .20,.15,.10 -c .30,.25,.20"
+    $check_users = "/usr/lib/nagios/plugins/check_users -w 5 -c 10"
+  } elsif $hostname == 'app-b' {
+    $check_total_procs = "/usr/lib/nagios/plugins/check_procs -w 180 -c 220"
+    $check_users = "/usr/lib/nagios/plugins/check_users -w 5 -c 10"
+    $check_load = "/usr/lib/nagios/plugins/check_load -r -w .15,.10,.05 -c .30,.25,.20"
+  } else {
+    $check_users = "/usr/lib/nagios/plugins/check_users -w 5 -c 10"
+    $check_load = "/usr/lib/nagios/plugins/check_load -r -w .15,.10,.05 -c .30,.25,.20"
+    $check_total_procs = "/usr/lib/nagios/plugins/check_procs -w 150 -c 200"
+  }
+  
   file {"/etc/nagios/nrpe.cfg":
     ensure => present,
-    source => "puppet:///modules/nrpe_server/nrpe.cfg",
+    content => template("nrpe_server/nrpe.cfg.erb"),
     mode => "0444",
     owner => "root",
     group => "root",
     require => Class["nrpe_server::install"],
     notify => Class["nrpe_server::service"],
   }
+  
   file {"/usr/lib/nagios/plugins/logged_users.sh":
     ensure => present,
     source => "puppet:///modules/nrpe_server/logged_users.sh",
