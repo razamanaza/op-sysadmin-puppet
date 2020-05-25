bce10b3e8aea49a043373feee0adcead9f57ff75
root <root@mgmt-b.foo.org.nz>
2020-05-25 12:10:50 +1200

    Changed IP addresses to hostnames

--- modules/bkp/files/app_bkp.sh
+++ modules/bkp/files/app_bkp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -azP --delete /var/www/ b@10.25.137.160:/home/b/bkp/app
+rsync -azP --delete /var/www/ b@storage.foo.org.nz:/home/b/bkp/app
 if [ "$?" -ne "0" ]
 then
   touch /home/student/bkpfail
--- modules/bkp/files/db_bkp.sh
+++ modules/bkp/files/db_bkp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BKPDIR=/home/student/bkp
-mysqldump --all-databases --add-drop-table > $BKPDIR/mariadb-bkb.sql && rsync -azP --delete $BKPDIR/ b@10.25.137.160:/home/b/bkp/db
+mysqldump --all-databases --add-drop-table > $BKPDIR/mariadb-bkb.sql && rsync -azP --delete $BKPDIR/ b@storage.foo.org.nz:/home/b/bkp/db
 if [ "$?" -ne "0" ]
 then
   touch /home/student/bkpfail
