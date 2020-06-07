2af9c2c3128fcbd88ce7c5755b6e27bbee004a46
root <root@mgmt-b.foo.org.nz>
2020-06-08 09:00:18 +1200

    Add tcp6 default block rule

--- modules/fw/manifests/init.pp
+++ modules/fw/manifests/init.pp
@@ -2,6 +2,9 @@ class fw {
   resources { 'firewall':
     purge => true,
   }
+  resources { 'firewallchain':
+    purge => true,
+  }
 
   Firewall {
     before  => Class['fw::post'],
--- modules/fw/manifests/post.pp
+++ modules/fw/manifests/post.pp
@@ -1,8 +1,14 @@
 class fw::post {
-	firewall { '999 drop all':
-  	proto  => 'all',
+  firewall { '999 drop all other requests':
+    proto => 'all',
     action => 'drop',
     before => undef,
-   }
+  }
+  firewall { '998 drop all ipv6 requests':
+    action => 'drop',
+    provider => 'ip6tables',
+    proto => 'all',
+    before => undef,
+  }
 }
  
